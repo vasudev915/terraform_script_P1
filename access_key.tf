bccbbce7dec46fa58e71cad358c4307ab85f6927
vasudev <103812191+vasudev915@users.noreply.github.com>
2023-02-22 16:21:20 +0530

    Update access_key.tf

--- access_key.tf
+++ access_key.tf
@@ -1,5 +1,5 @@
 provider "aws" {
   region     = "us-east-1"
-  access_key = "AKIAU5E5PQTN2C6ULFV6"
-  secret_key = "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"
+  access_key = ""
+  secret_key = ""
 }
